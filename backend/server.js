3cc842bae3f52840d067dc8d52cb443214025156
gowthamnanba <gowthamkumar.rk@gmail.com>
2021-10-29 00:46:37 +0530

    Checking

--- backend/server.js
+++ backend/server.js
@@ -8,9 +8,9 @@ const cors=require('cors')
 dotenv.config()
 app.use(express.json())
 
-mongoose.connect(process.env.ATLAS_URI,()=>console.log("Db Connceted"))
+//mongoose.connect(process.env.ATLAS_URI,()=>console.log("Db Connceted"))
 
 app.use(express.json())
 app.use(cors())
 app.use('/api',routesUrl)
-app.listen(6000,()=> console.log("Server Ons"))
+app.listen(6000,()=> console.log("Server On"))
